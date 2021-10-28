042c0bc7c7e261c1d3901166fab168a4f661ba09
senn <bart.senn@gmail.com>
2021-10-28 13:46:15 +0200

    plugin constants

---
+++ src/com/senn/aem/plugin/intellij/compgen/PluginConstants.java
@@ -0,0 +1,11 @@
+package com.senn.aem.plugin.intellij.compgen;
+
+/**
+ * General plugin wide constants
+ * @author bart.senn@gmail.com
+ */
+public interface PluginConstants {
+
+    String PLUGIN_ID = "com.senn.aem.plugin.intellij.compgen";
+
+}
